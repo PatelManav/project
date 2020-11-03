5f94f7298fade46469fbc37e72e58198e3f5f617
PatelManav <manav.p@ahduni.edu.in>
2020-11-03 13:34:07 +0530

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+interface Todo {
+  title: String;
+  indent: Number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'proj';
+  title = 'DragDrop';
+  standards = [];
+
+  drop(event: CdkDragDrop<Todo[]>) {
+    var indent = this.standards[event.previousIndex].indent;
+    moveItemInArray(this.standards, event.previousIndex, event.currentIndex);
+    if (event.previousIndex < event.currentIndex) {
+      while (this.standards[event.previousIndex].indent > indent) {
+        moveItemInArray(
+          this.standards,
+          event.previousIndex,
+          event.currentIndex
+        );
+      }
+    } else {
+      let i = 1;
+      while (this.standards[event.previousIndex + i].indent > indent) {
+        moveItemInArray(
+          this.standards,
+          event.previousIndex + i,
+          event.currentIndex + i
+        );
+        i++;
+      }
+    }
+  }
+
+  add() {
+    var indent = 0;
+    if (this.standards.length - 1 >= 0) {
+      indent = this.standards[this.standards.length - 1].indent;
+    }
+    this.standards.push({ title: 'Add new standard here', indent: indent });
+  }
+
+  delete(index) {
+    this.standards.splice(index, 1);
+  }
+
+  indent(index) {
+    this.standards[index].indent += 1;
+  }
+
+  outdent(index) {
+    this.standards[index].indent -= 1;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, DragDropModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
